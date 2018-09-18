d90613dbba4e3c326f58353514663e8b64d2b4e9
fahad rasheed <fahad@altitude.com.my>
2018-09-18 13:09:32 +0500

    changed
    invoices
    
    Co-Authored-By: Hamza Mehmood <hamza_mehmood40@hotmail.com>

--- src/pages/epaytab/epaytab.ts
+++ src/pages/epaytab/epaytab.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams, Platform, AlertController, LoadingController,
 import { RestProvider } from '../../providers/rest/rest';
 import { EpaypaymentdetailPage } from '../epaypaymentdetail/epaypaymentdetail';
 import { EpayinvoicedetailPage } from '../epayinvoicedetail/epayinvoicedetail';
+import { LoginPage } from '../login/login';
 
 import { Http} from '@angular/http';
 
@@ -23,9 +24,12 @@ export class EpaytabPage {
   public headers:any;
   public page_number:any;
   public invoices_list:any;
+  public outstanding:any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform,public alertCtrl: AlertController, public http:Http, public loadingCtrl: LoadingController,private app: App, private modalCtrl: ModalController) 
   {
     this.page_number=0;
+    this.outstanding=0;
+    this.invoices_list=[];
     this.currency=window.localStorage.getItem('currency');
     this.url='http://staging.irisk.my/api/v3/';
     this.resident_id=window.localStorage.getItem('resident_id');
@@ -33,13 +37,13 @@ export class EpaytabPage {
     this.unit_id=window.localStorage.getItem('unit_id');
     this.condo_id=window.localStorage.getItem('condo_id');
     platform.ready().then(() => {  
-      this.get_deposit_detail();
+      this.get_invoices_detail();
      
     });
 
    
   }
-  get_deposit_detail(){
+  get_invoices_detail(){
     let loading = this.loadingCtrl.create({
       content: 'Loading invoices ...'
     });
@@ -47,21 +51,28 @@ export class EpaytabPage {
     var headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
     return new Promise(resolve=>{
-      this.http.get(this.url + 'get_my_invoices/'+ this.resident_id+'/'+ this.condo_id+'/'+this.unit_id+'/'+this.post_id+'/'+this.key+'/'+this.page_number,{headers: this.headers}).subscribe(data=>{
+      this.http.get(this.url + 'get_my_invoices/'+ this.resident_id+'/'+ this.condo_id+'/'+this.unit_id+'/'+this.key+'/'+this.page_number,{headers: this.headers}).subscribe(data=>{
         console.log(data.json());
-        if(data.json().status=='success')
+        if(data.json().errorCode==0)
         {
           console.log("SUCCESS");
           console.log(data.json().data);
           this.invoices_list=data.json().data;
+          this.outstanding=data.json().oustanding;
           this.noneresult = false;
           loading.dismiss();
-        }else if(data.json().status=='failed'){
+        }else if(data.json().errorCode==1){
           console.log("FAILED");
           console.log(data.json().data);
           this.noneresult = true;
           loading.dismiss();
           console.log("No Data Found");
+        }
+        else if(data.json().errorCode==2){
+          loading.dismiss();
+          this.show_errorkey_alert("Invalid key");
+          console.log("ERROR IN SERVER");
+          this.noneresult = true;
         }
        else
        resolve(false);
@@ -76,6 +87,70 @@ export class EpaytabPage {
        });
  
    });
+    }
+    doInfinite(infiniteScroll:any) {
+        this.page_number++;
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+      return new Promise(resolve=>{
+        this.http.get(this.url + 'get_my_invoices/'+ this.resident_id+'/'+ this.condo_id+'/'+this.unit_id+'/'+this.key+'/'+this.page_number,{headers: this.headers}).subscribe(data=>{
+          console.log(data.json());
+          if(data.json().errorCode==0)
+          {
+        
+            this.invoices_list = (this.invoices_list).concat(data.json().data);
+            infiniteScroll.complete();
+                  }
+                  else
+                     
+              infiniteScroll.enable(false);
+              },onerror=>{ 
+
+          infiniteScroll.complete();
+       
+        });
+          });
+  }
+    show_error_alert(des)
+    {
+      let alert = this.alertCtrl.create({
+        
+        //subTitle: "PURPOSE OF DEPOSIT",
+        message: des,
+      //  message: "<ion-item><p style='overflow:auto;white-space:normal;'>Test</p> <button ion-button outline item-right icon-left (click)='itemSelected()'><ion-icon name='eye'></ion-icon>View</button>",
+        buttons: [
+             {
+               text: 'Close',
+               handler: () => {
+              this.navCtrl.setRoot();
+               }
+             }
+           ]
+       });
+                     
+       alert.present();
+    
+    }
+    show_errorkey_alert(des)
+    {
+      let alert = this.alertCtrl.create({
+        
+        //subTitle: "PURPOSE OF DEPOSIT",
+        message: des,
+      //  message: "<ion-item><p style='overflow:auto;white-space:normal;'>Test</p> <button ion-button outline item-right icon-left (click)='itemSelected()'><ion-icon name='eye'></ion-icon>View</button>",
+        buttons: [
+             {
+               text: 'Close',
+               handler: () => {
+                window.localStorage.clear();
+                this.app.getRootNav().setRoot(LoginPage);
+               }
+             }
+           ]
+       });
+                     
+       alert.present();
+    
     }
   paymentdetail(){
     this.navCtrl.push(EpaypaymentdetailPage);
